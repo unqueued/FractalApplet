70144349d12df8ccdb7cfc634414e79ae50357e8
Timothy Sanders <ts1999@nyu.edu>
2017-12-03 13:38:24 -0500

    Stored metadata

---
+++ restoretouch.sh
@@ -0,0 +1,9 @@
+
+touch -h -t 201712031338.14 "."
+touch -h -t 201712031337.45 "./.git"
+touch -h -t 200912161256.36 "./Fractal.java"
+touch -h -t 200912110621.46 "./FractalApplet-initial.java"
+touch -h -t 200912161116.09 "./FractalApplet.java"
+touch -h -t 200912161213.54 "./index.html"
+touch -h -t 200207151347.58 "./Mandelbrot.java"
+touch -h -t 201712031338.14 "./restoretouch.sh"
